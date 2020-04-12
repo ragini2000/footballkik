67752e95b5de82bdf6e13e017740e5f4f44cc77c
ragini2000 <rlragini2000@gmail.com>
2020-04-12 16:01:17 +0530

    Facebook button added

--- model/user.js
+++ model/user.js
@@ -8,7 +8,9 @@ const userSchema=mongoose.Schema({
     password:{type:String,unique:true,default:""},//we add default here since if user wants to login through fb or gmail, we dont need him to enter pasword
     UserImage:{type:String,default:"default.png"},//in database, we only store the name of the image and not the image itself, image is stored in AWS bucket
     facebook:{type:String,default:""},
-    fbToken:Array
+    fbToken:Array,
+    google:{type:String,default:""},
+    googleTokens:Array
 
 });
 
