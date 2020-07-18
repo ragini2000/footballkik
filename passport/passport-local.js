5dc8bc8bf322acbad570fb3f607f82abb2495300
ragini2000 <rlragini2000@gmail.com>
2020-07-18 16:35:25 +0530

    now More than 1 user can be added in DB

--- passport/passport-local.js
+++ passport/passport-local.js
@@ -4,7 +4,7 @@ const passport=require("passport");
 const User=require("../model/user");
 const LocalStrategy=require("passport-local").Strategy;
 
-passport.serializeUser(function(user, done) {//serializeUser determines which of the users data will be saved in this session, here we save userID
+passport.serializeUser((user, done)=> {//serializeUser determines which of the users data will be saved in this session, here we save userID
     done(null, user.id);
   });
   
@@ -14,26 +14,31 @@ passport.deserializeUser(function(id, done) {//deserializeUser is used to retriv
     });
 });
 
-passport.use("local.signup",new LocalStrategy({//this is the passport middleware
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback:true//all of the users data will be passed into the callback
-  },(req, email, password, done)=> {
-    User.findOne({ "email": email }, (err, user)=> {
-        if (err) { return done(err); }//This err contains the error caused due to network erroe or something else
-        if (user) {
-          return done(null, false,req.flash("error","Email already exist"));//if user data exist in database
-        }
-          //Save this new user in database
-          var newUser=new User();// we get data such as username,email etc from forms using the body-parser in server.js file
-          newUser.username=req.body.username;
-          newUser.email=req.body.email;
-          newUser.password=newUser.encryptPassword(req.body.password);   
-          
-          newUser.save(err,function(){//to save the deatils provided in database
-              done(null,newUser);
-          });
-    });
+passport.use('local.signup', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, (req, email, password, done) => {
+  
+  User.findOne({'email': email}, (err, user) => {
+     if(err){
+         return done(err);
+     }
+      
+      if(user){
+          return done(null, false, req.flash('error', 'User with email already exist'));
+      }
+      
+      const newUser = new User();
+      newUser.username = req.body.username;
+      newUser.fullname = req.body.username;
+      newUser.email = req.body.email;
+      newUser.password = newUser.encryptPassword(req.body.password);
+      
+      newUser.save((err) => {
+          done(null, newUser);
+      });
+  });
 }));
 
 passport.use("local.login",new LocalStrategy({
