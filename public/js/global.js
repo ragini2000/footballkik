8800e2714e800cc9ad24081726af7655099c3351
ragini2000 <rlragini2000@gmail.com>
2020-07-29 18:41:42 +0530

    Online friends showing in group page- using global room

--- public/js/global.js
+++ public/js/global.js
@@ -14,4 +14,25 @@ $(document).ready(function(){
             img: img
         });
     });
+
+    socket.on('loggedInUser', function(users){//to listen loggedInUser event emitted from socket/globalroom.js
+        
+        var friends = $('.friend').text();//we want to get text from the div which is hidden(class name friend) friends="@friend1""@friend2"
+        var friend = friends.split('@');//split the above retrieved text at '@'  then friend=["@","friend1","friend2"]
+        
+        var name = $('#name-user').val().toLowerCase();
+        var ol = $('<div></div>');
+        var arr = [];//will contain friends that are online
+        //loops through the user array coming from server side and checks if the user with the given name property is found inside the 
+        //friend array then push that user into array and append the name to ol which contains empty div 
+        for(var i = 0; i < users.length; i++){
+        //indexof javascript method checks if the value exists inside an index and return the position of that value else returns -1
+            if(friend.indexOf(users[i].name) > -1){//we are searching for the above name (at line 21) inside the friend array
+                arr.push(users[i]);
+                ol.append(users[i].name);
+            }
+        }
+        $('#numOfFriends').text('('+arr.length+')');//in element with numofFriends i.e no. of friends online display it as --> (arr.length)
+        $('.onlineFriends').html(ol);
+    });
 });
